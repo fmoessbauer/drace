b239fe4783120689a3ca019001bf8ab3be44b246
Moessbauer <felix.moessbauer.ext@siemens.com>
2018-06-05 11:54:10 +0200

    Projektdateien hinzufügen.

---
+++ drace-client/drace-client.cpp
@@ -0,0 +1,12 @@
+﻿// drace-client.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "drace-client.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ drace-client/drace-client.h
@@ -0,0 +1,8 @@
+﻿// drace-client.h: Includedatei für Standardsystem-Includedateien
+// oder projektspezifische Includedateien.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Verweisen Sie hier auf zusätzliche Header, die das Programm benötigt.
