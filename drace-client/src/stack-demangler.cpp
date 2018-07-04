7a7d4f74523caccd686e7e1ccb714932ea17d4c5
Felix Moessbauer <felix.moessbauer.ext@siemens.com>
2018-07-04 10:32:25 +0200

    remove unused imports in stack demangler

--- drace-client/src/stack-demangler.cpp
+++ drace-client/src/stack-demangler.cpp
@@ -1,11 +1,8 @@
 #include "stack-demangler.h"
-#include "drace-client.h"
-#include "module-tracker.h"
 #include "symbols.h"
 
 #include <iostream>
 #include <string>
-#include <dr_api.h>
 
 void stack_demangler::demangle(void *stack) {
 	// get top element of stack
