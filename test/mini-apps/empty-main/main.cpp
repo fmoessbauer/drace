ac6ffe7943c46a8f15a9a6b858038675dd05d624
Felix Moessbauer <felix.moessbauer.ext@siemens.com>
2018-07-19 12:13:21 +0200

    increased complexity of example

--- test/mini-apps/empty-main/main.cpp
+++ test/mini-apps/empty-main/main.cpp
@@ -1,21 +1,27 @@
 #include <stdio.h>
 #include <thread>
 
-void fun() {
-	printf("Hello from Thread 1\n");
-	fflush(stdout);
+void fun(int * s) {
+	for (int i = 0; i < 1000; ++i) {
+		printf("Hello from Thread 1: Sence of Life: %i\n", *s);
+		fflush(stdout);
+	}
+
 }
-void fun2() {
-	printf("Hello from Thread 2\n");
-	fflush(stdout);
+void fun2(int * s) {
+	for (int i = 0; i < 1000; ++i) {
+		printf("Hello from Thread 2: Sence of Life: %i\n", *s);
+		fflush(stdout);
+	}
 }
 
 int main(int argc, char ** argv) {
+	int s = 42;
 	printf("Hello World\n");
 	fflush(stdout);
 
-	auto ta = std::thread(fun);
-	auto tb = std::thread(fun2);
+	auto ta = std::thread(fun, &s);
+	auto tb = std::thread(fun2, &s);
 	ta.join();
 	tb.join();
 
