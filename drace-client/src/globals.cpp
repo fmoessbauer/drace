29ae5e91f250011fc7c3bf8c31f6a2bc399f0899
Felix Moessbauer <felix.moessbauer.ext@siemens.com>
2018-10-24 15:50:04 +0200

    removed unused globals

--- drace-client/src/globals.cpp
+++ drace-client/src/globals.cpp
@@ -10,9 +10,8 @@
 /*
 * Thread local storage metadata has to be globally accessable
 */
-reg_id_t tls_seg;
-uint     tls_offs;
 int      tls_idx;
+std::unordered_map<thread_id_t, per_thread_t*> TLS_buckets;
 
 void *th_mutex;
 void *tls_rw_mutex;
@@ -22,14 +21,11 @@ drace::Config config;
 
 std::atomic<int> num_threads_active{ 0 };
 std::atomic<uint> runtime_tid{ 0 };
-std::unordered_map<thread_id_t, per_thread_t*> TLS_buckets;
 std::atomic<thread_id_t> last_th_start{ 0 };
 std::atomic<bool> th_start_pending{ false };
 
 std::chrono::system_clock::time_point app_start;
 std::chrono::system_clock::time_point app_stop;
-
-std::string config_file("drace.ini");
 
 std::unique_ptr<MemoryTracker> memory_tracker;
 std::unique_ptr<module::Tracker> module_tracker;
