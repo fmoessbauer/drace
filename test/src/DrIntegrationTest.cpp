1d0d7cd4388ab05506b4c496a79c9c9888f85707
Felix Moessbauer <felix.moessbauer@siemens.com>
2019-03-11 16:29:49 +0100

    test: re-enabled racy-atomics test

--- test/src/DrIntegrationTest.cpp
+++ test/src/DrIntegrationTest.cpp
@@ -34,10 +34,9 @@ TEST_P(FlagMode, EmptyMain) {
 TEST_P(FlagMode, Atomics) {
 	run(GetParam(), "mini-apps/atomics/gp-atomics.exe", 0, 0);
 }
-//TODO: check why this test hangs/deadlocks sometimes
-//TEST_P(FlagMode, RacyAtomics) {
-//	run(GetParam(), "mini-apps/atomics/gp-atomics.exe racy", 1, 10);
-//}
+TEST_P(FlagMode, RacyAtomics) {
+	run(GetParam(), "mini-apps/atomics/gp-atomics.exe racy", 1, 10);
+}
 TEST_P(FlagMode, Annotations) {
 	run(GetParam(), "mini-apps/annotations/gp-annotations.exe", 0, 0);
 }
