0f119b37fb578188792e23bb1a21087617393349
Felix Moessbauer <felix.moessbauer@siemens.com>
2019-03-14 09:26:50 +0100

    chore: build dummy detector in CI

--- contrib/build-ci.bat
+++ contrib/build-ci.bat
@@ -1,5 +1,13 @@
-echo "Build DRace"
+echo "== Build DRace ==="
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat" -arch=amd64 -host_arch=amd64
-cd build
+
+echo "=== Build drace-TSAN ==="
+cd build-tsan
 ninja
+cd ..
+
+echo "=== Build drace-Dummy ==="
+cd build-dummy
+ninja
+cd ..
