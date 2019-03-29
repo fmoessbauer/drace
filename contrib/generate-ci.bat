f5431bc1ac78a884c3d66d2d20dde1128deaa86c
Felix Moessbauer <felix.moessbauer@siemens.com>
2019-03-29 16:03:04 +0100

    chore: install target in CI

--- contrib/generate-ci.bat
+++ contrib/generate-ci.bat
@@ -7,23 +7,24 @@ set CXX=cl.exe
 echo "=== Generate drace-TSAN DR 7.0.x ==="
 mkdir build-tsan-dr70
 cd build-tsan-dr70
-cmake -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDRACE_XML_EXPORTER=1 -DDRACE_ENABLE_TESTING=1 -DDRACE_ENABLE_BENCH=1 -DDynamoRIO_DIR=C:/opt/DynamoRIO-Windows-7.0.17837-0/cmake -DDRACE_DETECTOR=tsan ..
+cmake -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDRACE_XML_EXPORTER=1 -DDRACE_ENABLE_TESTING=1 -DDRACE_ENABLE_BENCH=1 -DDynamoRIO_DIR=C:/opt/DynamoRIO-Windows-7.0.17837-0/cmake -DDRACE_DETECTOR=tsan -DCMAKE_INSTALL_PREFIX:PATH=package ..
 cd ..
 
 echo "=== Generate drace-TSAN DR 7.1.x ==="
 mkdir build-tsan-dr71
 cd build-tsan-dr71
-cmake -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDRACE_XML_EXPORTER=1 -DDRACE_ENABLE_TESTING=1 -DDRACE_ENABLE_BENCH=1 -DDynamoRIO_DIR=C:/opt/DynamoRIO-Windows-7.1.17963-0/cmake -DDRACE_DETECTOR=tsan ..
+cmake -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDRACE_XML_EXPORTER=1 -DDRACE_ENABLE_TESTING=1 -DDRACE_ENABLE_BENCH=1 -DDynamoRIO_DIR=C:/opt/DynamoRIO-Windows-7.1.17963-0/cmake -DDRACE_DETECTOR=tsan -DCMAKE_INSTALL_PREFIX:PATH=package ..
 cd ..
 
 echo "=== Generate drace-LEGACY-TSAN DR 7.1.x ==="
 mkdir build-legacy-tsan-dr71
 cd build-legacy-tsan-dr71
-cmake -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDRACE_USE_LEGACY_API=1 -DDRACE_ENABLE_TESTING=1 -DDRACE_ENABLE_BENCH=0 -DDynamoRIO_DIR=C:/opt/DynamoRIO-Windows-7.1.17963-0/cmake -DDRACE_DETECTOR=tsan ..
+cmake -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDRACE_USE_LEGACY_API=1 -DDRACE_ENABLE_TESTING=1 -DDRACE_ENABLE_BENCH=0 -DDynamoRIO_DIR=C:/opt/DynamoRIO-Windows-7.1.17963-0/cmake -DDRACE_DETECTOR=tsan -DCMAKE_INSTALL_PREFIX:PATH=package ..
 cd ..
 
 echo "=== Generate drace-Dummy ==="
 mkdir build-dummy
 cd build-dummy
-cmake -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDRACE_XML_EXPORTER=1 -DDRACE_ENABLE_TESTING=1 -DDRACE_ENABLE_BENCH=1 -DDynamoRIO_DIR=C:/opt/DynamoRIO-Windows-7.0.17837-0/cmake -DDRACE_DETECTOR=dummy ..
+cmake -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDRACE_XML_EXPORTER=1 -DDRACE_ENABLE_TESTING=1 -DDRACE_ENABLE_BENCH=1 -DDynamoRIO_DIR=C:/opt/DynamoRIO-Windows-7.0.17837-0/cmake -DDRACE_DETECTOR=dummy -DCMAKE_INSTALL_PREFIX:PATH=package ..
 cd ..
+
