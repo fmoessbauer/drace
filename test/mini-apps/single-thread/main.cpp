2a038ca35e82e7be7d93246e565d79f36415c184
Felix Moessbauer <felix.moessbauer@siemens.com>
2019-05-10 17:53:13 +0200

    feat: added single threaded dummy application

---
+++ test/mini-apps/single-thread/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * DRace, a dynamic data race detector
+ *
+ * Copyright 2018 Siemens AG
+ *
+ * Authors:
+ *   Felix Moessbauer <felix.moessbauer@siemens.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+int main() {
+	return 0;
+}
