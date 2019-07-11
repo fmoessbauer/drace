fee0bb96a3c140a00de9099c188cab6e0900e1dd
Felix Moessbauer <felix.moessbauer@siemens.com>
2019-07-11 14:06:34 +0200

    chore: fixed CI build pathes

--- contrib/build-ci.bat
+++ contrib/build-ci.bat
@@ -3,13 +3,13 @@ echo "== Build DRace ==="
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat" -arch=amd64 -host_arch=amd64
 
 echo "=== Build drace-TSAN DR 7.91.x ==="
-cd build-tsan-dr790
+cd build-tsan-dr791
 cmake --build .
 cmake --build . --target install
 cd ..
 
 echo "=== Build drace-LEGACY-TSAN DR 7.91.x ==="
-cd build-legacy-tsan-dr790
+cd build-legacy-tsan-dr791
 cmake --build .
 cmake --build . --target install
 cd ..
--- contrib/generate-ci.bat
+++ contrib/generate-ci.bat
@@ -5,14 +5,14 @@ set CC=cl.exe
 set CXX=cl.exe
 
 echo "=== Generate drace-TSAN DR 7.91.x ==="
-mkdir build-tsan-dr790
-cd build-tsan-dr790
+mkdir build-tsan-dr791
+cd build-tsan-dr791
 cmake -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDRACE_XML_EXPORTER=1 -DDRACE_ENABLE_TESTING=1 -DDRACE_ENABLE_BENCH=1 -DDynamoRIO_DIR=C:/opt/DynamoRIO-Windows-7.91.18081-0/cmake -DDRACE_DETECTOR=tsan -DCMAKE_INSTALL_PREFIX:PATH=package ..
 cd ..
 
 echo "=== Generate drace-LEGACY-TSAN DR 7.91.x ==="
-mkdir build-legacy-tsan-dr790
-cd build-legacy-tsan-dr790
+mkdir build-legacy-tsan-dr791
+cd build-legacy-tsan-dr791
 cmake -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDRACE_USE_LEGACY_API=1 -DDRACE_ENABLE_TESTING=1 -DDRACE_ENABLE_BENCH=0 -DDynamoRIO_DIR=C:/opt/DynamoRIO-Windows-7.91.18081-0/cmake -DDRACE_DETECTOR=tsan -DCMAKE_INSTALL_PREFIX:PATH=package ..
 cd ..
 
