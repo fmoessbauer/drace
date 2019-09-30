23b5830c7ee4c6f2f5a2f57ee1150070fdc5b9e7
Philip Harr <philip.harr@siemens.com>
2019-09-30 11:29:14 +0200

    feat: further progress on drace gui

--- tools/DRaceGUI/draceGUI.cpp
+++ tools/DRaceGUI/draceGUI.cpp
@@ -1,13 +1,16 @@
 #include "draceGUI.h"
 #include "./ui_draceGUI.h"
 
-//#include <windows.h>
 
 DRaceGUI::DRaceGUI(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::DRaceGUI)
 {
     ui->setupUi(this);
+
+//set some default values
+    command[detector_pos] = "-d tsan";
+    ui->dr_path_input->setText("drrun.exe");
 }
 
 DRaceGUI::~DRaceGUI()
@@ -18,76 +21,155 @@ DRaceGUI::~DRaceGUI()
 
 void DRaceGUI::on_dr_path_btn_clicked()
 {
-    QString path = QFileDialog::getOpenFileName(this, "Open File", "C:\\");
+    QString path = QFileDialog::getOpenFileName(this, "Open File", dr_pth_cache);
+    dr_pth_cache = path;
     ui->dr_path_input->setText(path);
-    updateCommand(path, 0);
-
 }
 
 void DRaceGUI::on_drace_path_btn_clicked()
 {
-     QString path = QFileDialog::getOpenFileName(this, "Open File", "C:\\");
+     QString path = QFileDialog::getOpenFileName(this, "Open File", drace_pth_cache);
+     drace_pth_cache = path;
      ui->drace_path_input->setText(path);
-     path = "-c " + path;
-     updateCommand(path, 1);
 }
 
 void DRaceGUI::on_exe_path_btn_clicked()
 {
-     QString path = QFileDialog::getOpenFileName(this, "Open File", "C:\\");
+     QString path = QFileDialog::getOpenFileName(this, "Open File", exe_pth_cache);
+     exe_pth_cache = path;
      ui->exe_input->setText(path);
-     updateCommand(path, 4);
 }
 
 void DRaceGUI::on_config_browse_btn_clicked()
 {
-     QString path = QFileDialog::getOpenFileName(this, "Open File", "C:\\");
+     QString path = QFileDialog::getOpenFileName(this, "Open File", config_pth_cache);
+     config_pth_cache = path;
      ui->config_file_input->setText(path);
-     path = "-c " + path;
-     updateCommand(path, 3);
 }
 
 void DRaceGUI::on_run_button_clicked()
 {
+    QString command = "start powershell -NoExit \"" + make_command() + "\"";
+    std::string str_command = command.toStdString();
+    STARTUPINFO info={sizeof(info)};
+    PROCESS_INFORMATION processInfo;
 
+    CreateProcessA(str_command.c_str(), "cmd", NULL, NULL, FALSE, 0, NULL, NULL, &info, &processInfo);
+
+    system(str_command.c_str());
 }
 
-
-void DRaceGUI::on_dr_path_input_textEdited(const QString &arg1)
+void DRaceGUI::make_entry(const QString &path, uint position, QString prefix)
 {
-    DRaceGUI::updateCommand(arg1, 0);
+    if(path != ""){
+        QString temp = path;
+        if(temp.contains(QRegExp("\\s+"))){
+            temp = "\"" + temp + "\"";
+        }
+        if(prefix != ""){
+            temp = prefix + " " + temp;
+        }
+        updateCommand(temp, position);
+    }
+    else{
+        updateCommand("", position);
+    }
+}
+
+void DRaceGUI::on_dr_path_input_textChanged(const QString &arg1)
+{
+    make_entry(arg1, dr_pos);
 
 }
+
+void DRaceGUI::on_drace_path_input_textChanged(const QString &arg1)
+{
+    make_entry(arg1, drace_pos, "-c");
+}
+
+void DRaceGUI::on_exe_input_textChanged(const QString &arg1)
+{
+    make_entry(arg1, exe_pos, "--");
+}
+
 
 void DRaceGUI::updateCommand(const QString &arg1, uint position){
     command[position] = arg1;
-    //ui->command_output->clear();
-    entire_command = "";
-    for(int it=0; it < 4; it++){
-        entire_command.append(command[it]);
-        entire_command.append(" ");
-    }
+
+    QString entire_command = make_command();
     ui->command_output->setText(entire_command);
 }
+
+QString DRaceGUI::make_command(){
+    QString entire_command = "";
+    for(int it=0; it < (exe_pos+1); it++){
+        if(command[it] != ""){
+            entire_command.append(command[it]);
+            entire_command.append(" ");
+        }
+    }
+    return entire_command;
+}
+
 
 void DRaceGUI::on_copy_button_clicked()
 {
     clipboard->setText(entire_command);
 }
 
+void DRaceGUI::make_flag_entry(const QString &arg1) {
+    QList<QString> split_string = arg1.split(QRegExp("\\s+")); //regex of one or many whitespaces
 
-void DRaceGUI::on_drace_path_input_textEdited(const QString &arg1)
-{
-    DRaceGUI::updateCommand(arg1, 1);
-}
-
-void DRaceGUI::on_exe_input_textEdited(const QString &arg1)
-{
-    DRaceGUI::updateCommand(arg1, 3);
+    QString to_append;
+    for (auto it = split_string.begin(); it != split_string.end(); it++) {
+        QString temp = *it;
+        if(temp.contains(" ")){
+            temp = "\"" + *it + "\"";
+        }
+        if(it == split_string.begin()){
+            to_append = temp;
+        }
+        else{
+            to_append += (" " + temp);
+        }
+    }
+    updateCommand(to_append, flags_pos);
 }
 
 void DRaceGUI::on_tsan_btn_clicked()
 {
-    QString detector = "-d tsan";
-     DRaceGUI::updateCommand(detector, 2);
+    make_entry("tsan", detector_pos, "-d");
+}
+
+void DRaceGUI::on_extsan_btn_clicked()
+{
+    make_entry("extsan", detector_pos, "-d");
+}
+
+void DRaceGUI::on_fasttrack_btn_clicked()
+{
+    make_entry("fasttrack", detector_pos, "-d");
+}
+
+
+void DRaceGUI::on_flag_input_textChanged(const QString &arg1)
+{
+    make_flag_entry(arg1);
+}
+
+void DRaceGUI::on_config_file_input_textChanged(const QString & arg1)
+{
+    make_entry(arg1, config_pos, "-c");
+}
+
+
+
+void DRaceGUI::on_dr_debug_stateChanged(int arg1)
+{
+    if(arg1 == 0){
+        updateCommand("", dr_debug_pos);
+    }
+    else{
+        updateCommand("-debug", dr_debug_pos);
+    }
 }
--- tools/DRaceGUI/draceGUI.h
+++ tools/DRaceGUI/draceGUI.h
@@ -1,6 +1,6 @@
 #ifndef DRACE_GUI_H
 #define DRACE_GUI_H
-
+#include <windows.h>
 #include <QMainWindow>
 #include <string>
 #include <QFileDialog>
@@ -12,15 +12,28 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class DRaceGUI; }
 QT_END_NAMESPACE
 
+
+
 class DRaceGUI : public QMainWindow
 {
+    static constexpr uint dr_pos = 0;
+    static constexpr uint dr_debug_pos = 1;
+    static constexpr uint drace_pos = 2;
+    static constexpr uint detector_pos = 3;
+    static constexpr uint flags_pos = 4;
+    static constexpr uint config_pos = 5;
+    static constexpr uint exe_pos = 6;
+
     Q_OBJECT
 
 public:
+    
     DRaceGUI(QWidget *parent = nullptr);
     ~DRaceGUI();
 
 private slots:
+    
+
     void on_dr_path_btn_clicked();
 
     void on_drace_path_btn_clicked();
@@ -31,26 +44,45 @@ private slots:
 
     void on_run_button_clicked();
 
+    void make_entry(const QString & path, uint postion, QString prefix = "");
 
-    void on_dr_path_input_textEdited(const QString &arg1);
+    void on_dr_path_input_textChanged(const QString &arg1);
 
     void updateCommand(const QString &arg1, uint position);
 
     void on_copy_button_clicked();
 
+    void make_flag_entry(const QString & arg1);
 
 
-    void on_drace_path_input_textEdited(const QString &arg1);
 
-    void on_exe_input_textEdited(const QString &arg1);
+    void on_drace_path_input_textChanged(const QString &arg1);
+
+    void on_exe_input_textChanged(const QString &arg1);
 
     void on_tsan_btn_clicked();
+    void on_fasttrack_btn_clicked();
+    void on_extsan_btn_clicked();
 
+
+    void on_flag_input_textChanged(const QString &arg1);
+
+    void on_config_file_input_textChanged(const QString &arg1);
+
+    void on_dr_debug_stateChanged(int arg1);
+
+    QString make_command();
 private:
+
+    QString DEFAULT_PTH =  "C:\\";
+    QString drace_pth_cache = DEFAULT_PTH;
+    QString dr_pth_cache = DEFAULT_PTH;
+    QString exe_pth_cache = DEFAULT_PTH;
+    QString config_pth_cache = DEFAULT_PTH;
+
     Ui::DRaceGUI *ui;
-    QString command[5];
+    QString command[(exe_pos+1)];
     QString entire_command;
     QClipboard *clipboard = QApplication::clipboard();
-
 };
 #endif // MAINWINDOW_H
