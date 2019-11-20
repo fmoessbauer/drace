4c9be969a99bc51b45859983cd535ecbad7003dc
Felix Moessbauer <felix.moessbauer@siemens.com>
2019-11-20 17:49:51 +0100

    chore: add support for vs enterprise

--- contrib/build-ci.bat
+++ contrib/build-ci.bat
@@ -1,6 +1,7 @@
 echo "== Build DRace ==="
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat" -arch=amd64 -host_arch=amd64 || goto :error
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat" -arch=amd64 -host_arch=amd64 || ^
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat" -arch=amd64 -host_arch=amd64
 
 echo "=== Build drace-TSAN DR 7.91.x ==="
 cd build-tsan-dr791
