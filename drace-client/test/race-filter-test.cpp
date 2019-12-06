7ac416f60240efe8892fb6ba91c5b98d53fca5b3
Felix Moessbauer <felix.moessbauer@siemens.com>
2019-12-06 11:08:01 +0100

    test: test race-supression component

--- drace-client/test/race-filter-test.cpp
+++ drace-client/test/race-filter-test.cpp
@@ -12,18 +12,44 @@
 #include "gtest/gtest.h"
 #include "race-filter-test.h"
 
+using ::testing::UnitTest;
+using drace::RaceFilter;
 
 /**
-  \todo
-  Implement Unit-Tests for the race-filter component
-*/
+ * Check the supression of an exact-match race
+ */
+TEST(FilterTest, StaticSupression) {
 
-using ::testing::UnitTest;
-TEST(FilterTest, dummy) {
+    std::string file = "race_suppressions.txt";
 
-    // std::string file = "race_suppressions.txt";
-    // drace::RaceFilter filter(file);
-    // filter.print_list();
+    // create fake detector race
+    Detector::Race r;
+    r.first.stack_size = 1;
+    r.first.stack_trace[0] = 0x42;
 
-    ASSERT_EQ(0,0);
+    // create dummy symbol
+    drace::symbol::SymbolLocation symbol_location;
+    symbol_location.file = "FilterTest.cpp";
+    symbol_location.mod_name = "FilterTestModule";
+		symbol_location.sym_name = "main";
+
+    // this should be supressed later
+    auto symbol_loc_lp = symbol_location;
+    symbol_loc_lp.sym_name = "std::_LaunchPad<>::_Go";
+
+    drace::race::ResolvedAccess ra(r.first);
+    drace::race::ResolvedAccess rb(r.second);
+    drace::race::ResolvedAccess r_launchpad(r.first);
+    ra.resolved_stack.push_back(symbol_location);
+    rb.resolved_stack.push_back(symbol_location);
+    r_launchpad.resolved_stack.push_back(symbol_loc_lp);
+
+    drace::race::DecoratedRace race_real(ra, rb, std::chrono::milliseconds(1000));
+    // create a false positive race
+    drace::race::DecoratedRace race_launchpad(r_launchpad, rb, std::chrono::milliseconds(1000));
+
+    RaceFilter filter(file);
+
+    ASSERT_FALSE(filter.check_suppress(race_real));
+    ASSERT_TRUE(filter.check_suppress(race_launchpad));
 }
