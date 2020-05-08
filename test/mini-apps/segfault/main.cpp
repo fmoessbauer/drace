de57243f260771b1b1491d11e3da12c61fcab59b
Felix Moessbauer <felix.moessbauer@siemens.com>
2020-05-08 12:07:09 +0200

    test: add linux support for segfault test

--- test/mini-apps/segfault/main.cpp
+++ test/mini-apps/segfault/main.cpp
@@ -9,6 +9,7 @@
  * SPDX-License-Identifier: MIT
  */
 
+#ifdef _WIN32
 #include <windows.h>
 #include <sstream>
 
@@ -18,9 +19,12 @@ LONG WINAPI TopLevelExceptionHandler(PEXCEPTION_POINTERS pExceptionInfo) {
   exit(1);
   return EXCEPTION_CONTINUE_SEARCH;
 }
+#endif
 
 int main() {
+#ifdef _WIN32
   SetUnhandledExceptionFilter(TopLevelExceptionHandler);
+#endif
   int *v = 0;
   v[12] = 0;  // trigger the fault
   return 0;
